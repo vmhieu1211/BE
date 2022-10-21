8986fe1a520ea88778cbde6aa00a81b5803629e4
vmhieu1211 <cavaldos1211@gmail.com>
2022-10-21 18:48:35 +0700

    unit01

---
+++ unit01/index1.php
@@ -0,0 +1,15 @@
+<?php
+//comment tren 1 dong 
+/* comment tren 
+
+nhieu dong 
+
+*/
+/*Php là 1 ngôn ngữ thông dịch 
+Nếu không dùng thẻ đóng => chỉ dùng php */
+
+echo "hello word";
+//echo is bettest
+
+
+?>
