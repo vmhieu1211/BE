75e4201a708499d89973284543f347bac425ba5d
vmhieu1211 <cavaldos1211@gmail.com>
2022-11-04 21:08:11 +0700

    11

--- covid/controller/Corona.php
+++ covid/controller/Corona.php
@@ -2,6 +2,7 @@
 if(!defined('APP_PATH')){
     die('can not access');
 }
+require 'model/Corona_model.php';
 $method=strtolower($_GET['m'] ?? 'index');
 switch($method){
     case 'index':
@@ -9,5 +10,11 @@ switch($method){
         break;
 }
 function Index(){
+    $virus=getDataVirusCorona();
+    $global = $virus['Global'] ?? [];
+    $countries = $virus['Countries'] ?? [];
+    //su dung global va $countries hien thi du lieu ngoai view
+    
+    
     require 'view/index_view.php';
 }
